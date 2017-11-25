6ba8dd91fb002d8b102f405ed1e3b00e6ef54540
Stas Kirichok <kirichok.stas@gmail.com>
2017-11-25 15:26:17 +0200

    Initial Commit

---
+++ ProtoBuf-Server/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProtoBuf-Server
+//
+//  Created by Rumata on 11/25/17.
+//  Copyright © 2017 Yalantis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
